5fe8f076577ba6c8ebee4a7fb59dff06eb73fce4
silverTerrace <145773304+silverTerrace@users.noreply.github.com>
2023-10-25 23:36:38 +0000

    Updated js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,72 @@ let squares = null;
 let board = ["", "", "", "", "", "", "", "", ""];
 let turn = ["X", "O"];
 let toggleSwitch = 0;
+let squareCount = 0;
+let button = null;
+
+function reset() {
+  button = document.querySelector("button");
+  //button.addEventListener("click", function());
+  board = ["", "", "", "", "", "", "", "", ""];
+  toggleSwitch = 0;
+  squareCount = 0;
+
+  squares = document.querySelectorAll("#board > div");
+
+  console.log(`board: ${board}`);
+  console.log(`t switch: ${toggleSwitch}`);
+  console.log(`squareCount: ${squareCount}`);
+  console.log(`squares: ${squares}`);
+
+  let statusElem = document.getElementById("status");
+  statusElem.innerHTML = `Move your mouse over a square and click to play an X or an O.`;
+  statusElem.classList.remove("you-won");
+
+  squares.forEach(function (square) {
+    square.classList.remove("X");
+    square.classList.remove("O");
+    square.classList.add("square");
+    square.innerHTML = "";
+    let squareNum = squareCount;
+    squareCount++;
+    let isclicked = false;
+    square.addEventListener("mouseover", function () {
+      square.classList.add("hover");
+    });
+    square.addEventListener("mouseout", function () {
+      square.classList.remove("hover");
+    });
+    square.addEventListener("click", function () {
+      isclicked
+        ? null
+        : (function () {
+            let player = turn[toggleSwitch % 2];
+            square.classList.toggle(player);
+            square.innerHTML = `${player}`;
+            board[squareNum] = `${player}`;
+            isclicked = true;
+            checkForWin(player)
+              ? (function () {
+                  statusElem.innerHTML = `Congratulations! ${player} is the Winner!`;
+                  statusElem.classList.add("you-won");
+                })()
+              : null;
+            console.log(
+              `Switch for move just played ${toggleSwitch} for player ${
+                turn[toggleSwitch % 2]
+              }\n`
+            );
+            toggleSwitch++;
+            console.log(
+              `if game continues, toggle is now ${toggleSwitch} and next player should be ${
+                turn[toggleSwitch % 2]
+              }`
+            );
+          })();
+    });
+    console.log(`squareninit: ${square}`);
+  });
+}
 
 // Define winning combinations
 const winningCombinations = [
@@ -17,23 +83,7 @@ const winningCombinations = [
   [2, 4, 6],
 ];
 
-document.addEventListener("DOMContentLoaded", function () {
-  squares = document.querySelectorAll("#board > div");
-  squares.forEach(function (square) {
-    square.classList.add("square");
-    let isclicked = false;
-    square.addEventListener("click", function () {
-      isclicked
-        ? null
-        : (function () {
-            square.classList.toggle(turn[toggleSwitch % 2]);
-            square.innerHTML = `${turn[toggleSwitch % 2]}`;
-            isclicked = true;
-            toggleSwitch++;
-          })();
-    });
-  });
-});
+document.addEventListener("DOMContentLoaded", reset);
 
 // Function to check for a win
 function checkForWin(player) {
